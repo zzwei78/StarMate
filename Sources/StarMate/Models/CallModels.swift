1d9011835709d35ace71ef34b900f163a7fbb879
zhengwei.zheng <zhengwei.zheng@brkg.com>
2026-04-01 17:59:38 +0800

    Fix: Add SwiftUI import to model files using Color

--- Sources/StarMate/Models/CallModels.swift
+++ Sources/StarMate/Models/CallModels.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 // MARK: - Call State
 enum CallState: Equatable {
--- Sources/StarMate/Models/DeviceModels.swift
+++ Sources/StarMate/Models/DeviceModels.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 // MARK: - BLE Connection State
 enum ConnectionState: Equatable {
